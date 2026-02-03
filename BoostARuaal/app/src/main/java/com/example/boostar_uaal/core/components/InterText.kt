ece0d9ad3231d7913d38319ca0f370c0f3ce4b75
marc.diaz.7e8 <marc.diaz.7e8@itb.cat>
2026-02-03 16:53:24 +0100

    Home Empezado

--- BoostARuaal/app/src/main/java/com/example/boostar_uaal/core/components/InterText.kt
+++ BoostARuaal/app/src/main/java/com/example/boostar_uaal/core/components/InterText.kt
@@ -1,11 +1,14 @@
 package com.example.boostar_uaal.core.components
 
+import androidx.annotation.ColorInt
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import com.example.boostar_uaal.R
@@ -15,13 +18,17 @@ fun InterText(
     text: String,
     modifier: Modifier = Modifier,
     fontSize: TextUnit = 16.sp,
-    fontWeight: FontWeight = FontWeight.Medium
+    fontWeight: FontWeight = FontWeight.Medium,
+    textAlign: TextAlign = TextAlign.Start,
+    color: Color = Color.Black
 ){
     Text(
         text = text,
         modifier = modifier,
         fontFamily = FontFamily(Font(R.font.inter_medium)),
         fontSize = fontSize,
-        fontWeight = fontWeight
+        fontWeight = fontWeight,
+        textAlign = textAlign,
+        color = color
     )
 }
--- BoostARuaal/app/src/main/java/com/example/boostar_uaal/ui/screen/homeScreen/HomeScreen.kt
+++ BoostARuaal/app/src/main/java/com/example/boostar_uaal/ui/screen/homeScreen/HomeScreen.kt
@@ -1,9 +1,70 @@
 package com.example.boostar_uaal.ui.screen.homeScreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.boostar_uaal.R
+
+import com.example.boostar_uaal.core.components.InterText
+import com.example.boostar_uaal.core.components.ItemCarrousel
+import com.example.boostar_uaal.core.theme.BoostARuaalTheme
+import com.example.boostar_uaal.core.theme.authPrimaryButtonColor
 import com.example.boostar_uaal.navigation.Routes
+
 
 @Composable
 fun HomeScreen(navigateTo: (Routes) -> Unit){
+    Column(Modifier.fillMaxSize()) {
+        Row(Modifier.fillMaxWidth()) {
+            InterText(
+                text = "Home",
+                fontSize = 38.sp
+            )
+        }
+        Row(Modifier.fillMaxWidth()) {
+            Card(
+                shape = CircleShape,
+                modifier = Modifier.width(90.dp)
+            ) {
+                InterText(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    text = "Para ti",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    color = authPrimaryButtonColor
 
+                )
+            }
+        }
+        Card() {
+            Image(
+                painter = painterResource(R.drawable.home),
+                contentDescription = "Home Hero"
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+        HomeScreen({})
 }
