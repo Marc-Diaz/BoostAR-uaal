19d9a179e8e1ba311a8834df514312c0f7335977
Mcarolinapc <mposliguacontreras@gmail.com>
2026-02-23 16:15:15 +0100

    error multimedia arreglado

--- BoostARuaal/app/src/main/java/com/example/boostar_uaal/ui/screen/feedScreen/FeedScreenViewModel.kt
+++ BoostARuaal/app/src/main/java/com/example/boostar_uaal/ui/screen/feedScreen/FeedScreenViewModel.kt
@@ -5,15 +5,20 @@ import com.example.core.entities.Product
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.boostar_uaal.core.entities.ProductDetail
 import com.example.boostar_uaal.data.repository.MockProductRepositoryImpl
+import kotlinx.coroutines.launch
 
 class FeedScreenViewModel : ViewModel() {
 
-    private val _product = MutableStateFlow<Product?>(null)
-    val product: StateFlow<Product?> = _product
+    private val _product = MutableStateFlow<ProductDetail?>(null)
+    val product: StateFlow<ProductDetail?> = _product
 
     fun getProduct(productId: Int) {
-        val product = MockProductRepositoryImpl().getProductById(productId)
-        _product.value = product
+        viewModelScope.launch {
+            val product = MockProductRepositoryImpl().getProductById(productId)
+            _product.value = product
+        }
     }
 }
