0ad080f1906b236a6c6c087f0abe6bbd24e4fa2e
Mcarolinapc <mposliguacontreras@gmail.com>
2026-02-24 16:14:12 +0100

    refactorizació codigo,

--- BoostARuaal/app/src/main/java/com/example/boostar_uaal/ui/screen/homeScreen/HomeScreen.kt
+++ BoostARuaal/app/src/main/java/com/example/boostar_uaal/ui/screen/homeScreen/HomeScreen.kt
@@ -1,14 +1,21 @@
 package com.example.boostar_uaal.ui.screen.homeScreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.boostar_uaal.ui.screen.homeScreen.components.ItemCarrousel
@@ -19,38 +26,45 @@ import com.example.boostar_uaal.ui.screen.homeScreen.components.HomeHero
 import com.example.boostar_uaal.ui.screen.homeScreen.components.HomeNav
 
 @Composable
-fun HomeScreen(navigateTo: (Routes) -> Unit){
+fun HomeScreen(navigateTo: (Routes) -> Unit) {
     val homeScreenViewModel = viewModel<HomeScreenViewModel>()
     val products by homeScreenViewModel.products.collectAsState()
 
     val scrollState = rememberScrollState()
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .verticalScroll(scrollState),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-        HomeHeader()
-        HomeNav()
-        HomeHero()
-        ItemCarrousel(
-            name = "Para ti",
-            products = products,
-            navigateTo = {navigateTo(Routes.HomeScreen)},
-            itemClickable = { navigateTo(Routes.FeedScreen(it)) }
-        )
-        ItemCarrousel(
-            name = "Tendencias",
-            products = products,
-            navigateTo = {navigateTo(Routes.HomeScreen)},
-            itemClickable = { navigateTo(Routes.FeedScreen(it)) }
-        )
+            .background(Color.White),
+        contentAlignment = Alignment.TopCenter
+    ) {
 
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .widthIn(max = 840.dp)
+                .background(Color.White)
+                .verticalScroll(scrollState)
+                .padding(bottom = 32.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            HomeHeader()
+            HomeNav()
+            HomeHero()
+            ItemCarrousel(
+                name = "Para ti",
+                products = products,
+                navigateTo = { navigateTo(Routes.HomeScreen) },
+                itemClickable = { navigateTo(Routes.FeedScreen(it)) }
+            )
+            ItemCarrousel(
+                name = "Tendencias",
+                products = products,
+                navigateTo = { navigateTo(Routes.HomeScreen) },
+                itemClickable = { navigateTo(Routes.FeedScreen(it)) }
+            )
+
+        }
     }
-}
-@Preview(showBackground = true)
-@Composable
-fun HomeScreenPreview(){
-        HomeScreen({})
 }
--- BoostARuaal/app/src/main/java/com/example/boostar_uaal/ui/screen/homeScreen/components/HomeHeader.kt
+++ BoostARuaal/app/src/main/java/com/example/boostar_uaal/ui/screen/homeScreen/components/HomeHeader.kt
@@ -1,10 +1,20 @@
 package com.example.boostar_uaal.ui.screen.homeScreen.components
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -12,13 +22,33 @@ import com.example.boostar_uaal.core.components.InterText
 
 @Composable
 fun HomeHeader() {
-    Row(Modifier.fillMaxWidth()
-        .padding(top = 45.dp)
-        .padding(horizontal = 24.dp)) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 16.dp),
+             verticalAlignment = Alignment.CenterVertically
+    ) {
         InterText(
             text = "Home",
-            fontSize = 38.sp,
+            fontSize = 38.54.sp,
             fontWeight = FontWeight.ExtraBold
         )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Surface(
+            shape = CircleShape,
+            color = Color(0xFFF2F2F2),
+            modifier = Modifier.size(48.dp)
+        ) {
+            IconButton(onClick = { /* Acción de búsqueda */ }) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Buscar",
+                    tint = Color.Gray,
+                    modifier = Modifier.size(28.dp)
+                )
+            }
+        }
     }
 }
